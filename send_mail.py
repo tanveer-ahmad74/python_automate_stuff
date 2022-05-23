44d32ee732b800548cb4149f207658a5fb5ca128
Tanveer-Ahmad <ta551682@gmail.com>
2022-05-23 12:34:29 +0500

    send_mail_python

---
+++ send_mail.py
@@ -0,0 +1,23 @@
+import smtplib, ssl
+
+def send_email():
+    port = 465
+    smtp_server = 'smtp.gmail.com'
+    sender_email = '@gmail.com'
+    receiver_email = '@gmail.com'
+    password = 'bbghnecifhopsbno'
+    message= 'this is the message for rana handsome guy'
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        try:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
+            print('email sent!')
+        except:
+            print("could not login or send the mail.")
+
+if __name__ == '__main__':
+        send_email()
+
+#Email that send from here probably find in SPAM folder
