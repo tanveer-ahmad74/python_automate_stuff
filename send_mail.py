6f4f8c63369d844769154455e46009487e8b700c
Tanveer <70311411+tanveer-ahmad74@users.noreply.github.com>
2022-05-27 13:16:20 +0500

    Add files via upload

---
+++ send_mail.py
@@ -0,0 +1,44 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def sendEmail(smtpHost, smtpPort, mailUname, mailPwd, fromEmail, mailSubject, mailContentHtml, recepientsMailList):
+    msg = MIMEMultipart()
+    msg['From'] = fromEmail
+    msg['To'] = ','.join(recepientsMailList)
+    msg['Subject'] = mailSubject
+    msg.attach(MIMEText(mailContentHtml, 'html'))
+
+
+    # Send message object as email using smptplib
+    s = smtplib.SMTP(smtpHost, smtpPort)
+    s.starttls()
+    s.login(mailUname, mailPwd)
+    msgText = msg.as_string()
+    sendErrs = s.sendmail(fromEmail, recepientsMailList, msgText)
+    s.quit()
+
+    # check if errors occured and handle them accordingly
+    if not len(sendErrs.keys()) == 0:
+        raise Exception("Errors occurred while sending email", sendErrs)
+
+
+# mail server parameters
+smtpHost = "smtp.gmail.com"
+smtpPort = 587
+mailUname = 'ta551682@gmail.com'
+mailPwd = 'vbhbrlafebiosbsp'
+fromEmail = 'usmaan.arsh908@gmail.com'
+
+
+# mail body, recepients, attachment files
+mailSubject = "Test subject"
+mailContentHtml = "Hi, Hope u are fine. <br/> This is a <b>test</b>" \
+                  "mail from python script using an awesome library called" \
+                  " <b>smtplib</b>"
+
+recepientsMailList = ["usmaan.arsh908@gmail.com"]
+sendEmail(smtpHost, smtpPort, mailUname, mailPwd, fromEmail,
+          mailSubject, mailContentHtml, recepientsMailList)
+
+print("execution complete...")
