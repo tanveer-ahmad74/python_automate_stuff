5ba7735b3dcce1829647ad2ba4eccb0a6d32a958
Tanveer <70311411+tanveer-ahmad74@users.noreply.github.com>
2022-05-31 12:02:10 +0500

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -1,6 +1,43 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import requests
+import keys
+import pandas as pd
+
+def get_crypto_rates(base_currency='PKR', assets='BTC,ETH,XRP'):
+    url = 'https://api.nomics.com/v1/currencies/ticker'
+
+    payload = {'key': keys.NOMICS_API_KEY, 'convert': base_currency, 'ids': assets, 'interval': '1d'}
+    response = requests.get(url, params=payload)
+    data = response.json()
+    print(data)
+    crypto_currency, crypto_price, crypto_timestamp = [], [], []
+
+    for asset in data:
+        crypto_currency.append(asset['currency'])
+        crypto_price.append(asset['price'])
+        crypto_timestamp.append(asset['price_timestamp'])
+
+    raw_data = {
+        "assets": crypto_currency,
+        'rates': crypto_price,
+        "timestamp": crypto_timestamp
+    }
+
+    df = pd.DataFrame(raw_data)
+    return df
+
+def set_alert(dataframe, asset, alert_high_price):
+    crypto_value = float(dataframe[dataframe['assets'] == asset]['rates'].item())
+
+    details = f'{asset}: {crypto_value}, Target Value Reached: {alert_high_price}'
+
+    if crypto_value >= alert_high_price:
+        print(details + '<< Target Value Reached..!')
+        return details
+    else:
+        print(details)
 
 def sendEmail(smtpHost, smtpPort, mailUname, mailPwd, fromEmail, mailSubject, mailContentHtml, recepientsMailList):
     msg = MIMEMultipart()
@@ -31,11 +68,17 @@ mailPwd = 'vbhbrfadsfasdfp'    #APp_Passwprd_of_Gmail
 fromEmail = 'usmaan.arsh908@gmail.com' #to send
 
 
+df = get_crypto_rates()
+value1 = set_alert(df, 'BTC', 607064190.50)
+value2 = set_alert(df, 'ETH', 386128.92)
+value3 = set_alert(df, 'XRP', 80.00)
+
 # mail body, recepients, attachment files
 mailSubject = "Test subject"
-mailContentHtml = "Hi, Hope u are fine. <br/> This is a <b>test</b>" \
-                  "mail from python script using an awesome library called" \
-                  " <b>smtplib</b>"
+mailContentText = f"Hi...!?\n" \
+                  f"BTC: {value1}\n" \
+                  f"ETH: {value2}\n"  \
+                  f"XRP: {value3}"
 
 recepientsMailList = ["usmaan.arsh908@gmail.com"]
 sendEmail(smtpHost, smtpPort, mailUname, mailPwd, fromEmail,
